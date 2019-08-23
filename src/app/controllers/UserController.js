310e4b03defeedab4b77fe7039950a74709afe78
Pedro Bueno <pedro@mentores.com.br>
2019-08-23 00:18:52 -0300

    fix: removendo comentário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -49,8 +49,6 @@ class UserController {
 
     const user = await User.findByPk(req.userId);
 
-    console.log(req.userId, user.email);
-
     if (email !== user.email) {
       const userExists = await User.findOne({
         where: { email: req.body.email },
